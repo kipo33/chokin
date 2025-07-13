c366a6efb9d52c2c216b0a96e97b5a3a22fed4ea
Kipo <75076515+kipo33@users.noreply.github.com>
2025-07-13 15:20:51 +0900

    vite.config.tsを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// リポジトリ名を指定（あなたのGitHubユーザー名/リポジトリ名に変更してください）
-const repoName = 'kipo33/chokin'
+// リポジトリ名を指定
+const repoName = 'chokin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+  base: `/${repoName}/`,
 })
